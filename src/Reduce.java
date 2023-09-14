4f3d2fbb8ec894d607e5b7cb98586f3e0fe2817f
ably2003 <albert.li0133@gmail.com>
2023-09-13 21:41:51 -0400

    completed reduce untested

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,15 @@
 public class Reduce {
     public static void main(String[] args) {
-
+        int n = 100;
+        int count_steps = 0;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+            count_steps++;
+        }
+        System.out.println(count_steps);
     }
 }
